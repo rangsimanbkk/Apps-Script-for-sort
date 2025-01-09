f8fedee2adbafdbc79de0d91e72d81ed18013800
rangsimanbkk <rangsiman.p.bkk@gmail.com>
2025-01-09 13:30:34 +0700

    first commit

---
+++ Sort Google Sheet.js
@@ -0,0 +1,12 @@
+function SortonFormSubmit(e) {
+    const sheetId = "Your Sheet ID"; // Replace with your Google Sheet ID
+    const sheet = SpreadsheetApp.openById(sheetId).getActiveSheet();
+  
+    // Sort the range (excluding header)
+    const lastRow = sheet.getLastRow();
+    if (lastRow > 1) { // Only sort if there is more than just the header
+      Utilities.sleep(30000); //Wait 30s before sort
+      //sheet.getRange(row, column, numRows, numColumns).sort(columnPosition);
+      sheet.getRange(2, 1, lastRow - 1, sheet.getLastColumn()).sort(1);
+    }
+  }
