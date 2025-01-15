ac0985b256b417cbd036766e24810fbf5155dfd8
rangsimanbkk <rangsiman.p.bkk@gmail.com>
2025-01-15 15:55:51 +0700

    edit comment to explain variable

--- Sort Google Sheet.js
+++ Sort Google Sheet.js
@@ -6,7 +6,7 @@ function SortonFormSubmit(e) {
     const lastRow = sheet.getLastRow();
     if (lastRow > 1) { // Only sort if there is more than just the header
       Utilities.sleep(30000); //Wait 30s before sort
-      //sheet.getRange(row, column, numRows, numColumns).sort(columnPosition);
+      //sheet.getRange(row, column, numRows, numColumns).sort(columnPositionWantToSort);
       sheet.getRange(2, 1, lastRow - 1, sheet.getLastColumn()).sort(1);
     }
   }
